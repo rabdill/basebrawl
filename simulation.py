0924134d34666b4c58d5cb13a285c451f1caed04
Richard Abdill <rabdill@umn.edu>
2023-03-06 21:08:37 -0500

    Fixing rumble sim

--- simulation.py
+++ simulation.py
@@ -16,7 +16,8 @@ def Rumble(t1, t2, iterations):
         for player in t1.fighters + t2.fighters:
             player.reset()
 
-        results.append(fighting.Rumble(t1, t2, False))
+        z = fighting.Rumble(t1, t2)
+        results.append(z.winner)
     return(
-        (results.count(1), results.count(2))
+        (results.count(t1.name), results.count(t2.name))
     )
