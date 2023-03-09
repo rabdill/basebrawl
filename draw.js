b95ddc5ca9c67152cc1d469efdba50aabee0c26d
Richard Abdill <rabdill@umn.edu>
2023-03-08 22:47:23 -0500

    Displaying play by play

--- draw.js
+++ draw.js
@@ -1,3 +1,25 @@
+function Display_Rumble(team) {
+    display1 = document.getElementById('display1')
+    idx = Math.floor(Math.random() * data[team]['logs'].length)
+    toprint = data[team]['logs'][idx]
+    console.log(toprint)
+    // Print major events
+    display1.innerHTML = `
+        <h3>Overview</h3>
+        <ul>
+    `
+    for(entry of toprint[0]) {
+        display1.innerHTML += `<li>${entry}</li>`
+    }
+
+    display1.innerHTML += `
+        <h3>Play-by-play</h3>
+    `
+    for(entry of toprint[1]) {
+        display1.innerHTML += `<p>${entry}</p>`
+    }
+}
+
 function teamOverview(name, data, element) {
     teams = Object.keys(data)
     if(teams[0] == name) {
@@ -7,6 +29,7 @@ function teamOverview(name, data, element) {
     }
     element.innerHTML = `
         <h2>${name}: ${data[name]['wins']}&ndash;${data[opponent]['wins']}</h2>
+        <button type="button" class="btn btn-primary" onclick="Display_Rumble('${name}')">Show random win log</button>
         <ul>
     `
     for (var fighter of Object.keys(data[name]['fighter_records'])) {
